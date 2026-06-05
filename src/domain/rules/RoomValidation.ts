dc36e91a75b2b0469ce3d787d5d5e8513c51929c
Jeison Berdugo <jedabero20@gmail.com>
2026-06-04 19:26:14 -0500

    Implementar RoomValidation.validate

--- src/domain/rules/RoomValidation.ts
+++ src/domain/rules/RoomValidation.ts
@@ -2,6 +2,9 @@ import type { Room } from '../model/Room'
 
 export class RoomValidation {
   validate(room: Room): boolean {
-    throw new Error('not implemented')
+    if (!room.id) return false
+    if (!room.name) return false
+    if (!Number.isInteger(room.capacity) || room.capacity <= 0) return false
+    return true
   }
 }
