3e75317bf1d4ada684adfb760fc9740880a4bca4
Jeison Berdugo <jedabero20@gmail.com>
2026-06-04 19:28:41 -0500

    Coverage con vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,19 @@ export default defineConfig({
   test: {
     include: ['test/**/*.test.ts'],
     globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov'],
+      include: ['src/domain/**'],
+      exclude: [
+        'src/domain/**/index.ts',
+      ],
+      thresholds: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
+    },
   },
 })
